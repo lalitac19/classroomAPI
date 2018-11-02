b75bf9f29a5798e7bbaf3899f86c3965eccc56fc
lalitac19 <l.chopra17@gmail.com>
2018-11-02 12:43:24 +0000

    rest services added

---
+++ src/main/java/com/qa/rest/ClassroomEndpoint.java
@@ -0,0 +1,54 @@
+package com.qa.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.apache.log4j.Logger;
+
+import com.qa.business.service.ClassroomService;
+
+@Path("/classrooms")
+public class ClassroomEndpoint {
+	
+	@Inject
+	private ClassroomService service;
+
+	@Path("/getAllClassrooms")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllClassrooms() {
+		return service.getAllClassrooms();
+	}
+	
+	@Path("/getAClassroom/{id}")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllClassrooms(@PathParam("id") int id) {
+		return service.getAClassroom(id);
+	}
+
+	@Path("/addClassroom")
+	@POST
+	@Produces({ "application/json" })
+	public String addClassroom(String classroom) {
+		return service.addClassroom(classroom);
+	}
+
+	@Path("/deleteClassroom/{id}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String deleteMovie(@PathParam("id") int id) {
+		return service.deleteClassroom(id);
+	}
+
+	public void setService(ClassroomService service) {
+		this.service = service;
+	}
+
+}
